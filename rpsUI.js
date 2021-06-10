626abce159b10176b3ca95f0db6bbac984f87fb4
angiee39 <anjalarox@gmail.com>
2021-06-10 22:03:26 +0530

    Got a simple UI down, got the logic down, now need to end the game when either player reach score of 5, and declare overall winner.

--- rpsUI.js
+++ rpsUI.js
@@ -4,33 +4,18 @@ let playerScore = 0;
 // Computer score
 let compScore = 0;
 
-function playerPlay() {
-    
-    while (true)
-    {
-        let rawInput = window.prompt("Rock, Paper or Scissors?:");
-        let playerInput = rawInput.toLowerCase();
-        if (rawInput == "rock" || rawInput == "paper" || rawInput == "scissors")
-        {
-            return playerInput;
-            break;
-        }
-    }
-}
+let playerPlay;
 
 function computerPlay() {
 
     let ranNum = Math.floor(Math.random() * 3);
-    if (ranNum == 0)
-    {
+    if (ranNum == 0) {
         return ("rock");
     }
-    else if (ranNum == 1)
-    {
+    else if (ranNum == 1) {
         return ("paper");
     }
-    else
-    {
+    else {
         return ("scissors");
     }
 }
@@ -38,64 +23,69 @@ function computerPlay() {
 // Single round
 function playRound(playerSelection, computerSelection) {
     
-    playerSelection = playerPlay();
+    playerSelection = playerPlay;
     computerSelection = computerPlay();
     
-    if (playerSelection == "paper" && computerSelection == "rock")
-    {
+    if (playerSelection == "paper" && computerSelection == "rock") {
         playerScore++;
-        return("You Win! Paper beats Rock.");
+        return ("You Win! Paper beats Rock.")
     }
-    else if (playerSelection == "paper" && computerSelection == "scissors")
-    {
+    else if (playerSelection == "paper" && computerSelection == "scissors") {
         compScore++;
-        return("You lose! Scissors beat Paper.");
+        return ("You lose! Scissors beat Paper.")
     }
-    else if (playerSelection == "rock" && computerSelection == "paper")
-    {
+    else if (playerSelection == "rock" && computerSelection == "paper") {
         compScore++;
-        return("You Lose! Paper beats Rock.");
+        return ("You Lose! Paper beats Rock.")
     }
-    else if (playerSelection == "rock" && computerSelection == "scissors")
-    {
+    else if (playerSelection == "rock" && computerSelection == "scissors") {
         playerScore++;
-        return("You Win! Rock beats Scissors.");
+        return ("You Win! Rock beats Scissors.")
     }
-    else if (playerSelection == "scissors" && computerSelection == "rock")
-    {
+    else if (playerSelection == "scissors" && computerSelection == "rock") {
         compScore++;
-        return("You Lose! Rock beats Scissors.");
+        return ("You Lose! Rock beats Scissors.")
     }
-    else if (playerSelection == "scissors" && computerSelection == "paper")
-    {
+    else if (playerSelection == "scissors" && computerSelection == "paper") {
         playerScore++;
-        return("You Win! Scissors beats Paper.");
+        return ("You Win! Scissors beats Paper.")
     }
-    else if (playerSelection == computerSelection)
-    {
-        return ("It's a Draw!");
+    else if (playerSelection == computerSelection) {
+        return ("It's a Draw!")
     }
 }
 
 // Multiple rounds
 function game() {
 
-    for(let i = 0; i < 5; i++)
-    {
-        console.log(playRound());
-    }
-    if (playerScore > compScore)
-    {
-        console.log("You Win! Score is " + playerScore + ":" + compScore);
-    }
-    else if (playerScore < compScore)
-    {
-        console.log("You Lose! Score is " + playerScore + ":" + compScore);
-    }
-    else
-    {
-        console.log("It's a Draw! Score is " + playerScore + ":" + compScore);
-    }
+    main.textContent = playRound();
+    pc.textContent = playerScore;
+    cc.textContent = compScore;
+
+
+    // while (playerScore == 5 || compScore == 5)
+    // {
+    //     main.textContent = playRound();
+    //     pc.textContent = playerScore;
+    //     cc.textContent = compScore;
+    // }
 }
 
-game(playRound);
+
+rock.addEventListener('click', function (e) {
+    // console.log(e.target.innerText)
+    playerPlay = "rock"
+    game();
+});
+
+paper.addEventListener('click', function (e) {
+    // console.log(e.target.innerText)
+    playerPlay = "paper"
+    game();
+});
+
+scissors.addEventListener('click', function (e) {
+    // console.log(e.target.innerText)
+    playerPlay = "scissors"
+    game();
+});
